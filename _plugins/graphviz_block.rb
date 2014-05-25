94a7e7009e3a43618ce41bbd0bc97a194366cffa
Russell Harmon <russ@eatnumber1.com>
2014-05-25 00:53:11 -0400

    Update graphviz_block.rb

--- _plugins/graphviz_block.rb
+++ _plugins/graphviz_block.rb
@@ -1,52 +1,118 @@
-# (The MIT License)
-# 
-# Copyright © 2012-2013 Felix Ren-Chyan Chern
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy of
-# this software and associated documentation files (the ‘Software’), to deal in
-# the Software without restriction, including without limitation the rights to
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-# of the Software, and to permit persons to whom the Software is furnished to do
-# so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED ‘AS IS’, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# -*- coding:utf-8; mode:ruby; -*-
 
-# Modified from https://raw.github.com/dryman/dryman.github.com/src/plugins/graphviz_block.rb
-
-GRAPHVIZ_DIR = File.expand_path('../../source/images/graphviz', __FILE__)
-FileUtils.mkdir_p(GRAPHVIZ_DIR)
+['open3', 'win32/open3'].each do |lib|
+  begin
+    require lib
+    break
+  rescue LoadError
+  end
+end
 
 module Jekyll
-  class GraphvizBlock < Liquid::Raw
+  class GraphvizBlock < Liquid::Block
+
+    DIV_CLASS_ATTR = 'graphviz-wrapper'
+    DEFAULT_GRAPH_NAME = 'Graphviz'
+    DOT_OPTS = '-Tsvg'
+    DOT_EXEC = 'dot'
+    DOT_EXTS = (ENV['PATHEXT'] || '.exe;.bat;.com').split(";")
+    DOT_EXTS.unshift ''
+    DOT_PATH = ENV['PATH'].split(File::PATH_SEPARATOR)
+      .map{|a|File.join a, DOT_EXEC}
+      .map{|a|DOT_EXTS.map{|ex| a+ex}}.flatten
+      .find{|c|File.executable_real? c}
+    raise "not found a executable file: #{DOT_EXEC}" if DOT_PATH.nil?
+    DOT_CMD = "#{DOT_PATH} #{DOT_OPTS}"
+
+    def initialize(tag_name, markup, tokens)
+      super
+      @tag_name = tag_name
+
+      @title = markup or ""
+      @title.strip!
+
+      @src = ""
+    end
+
     def render(context)
-      code = super(context)
-      destination = File.join(
-        context.registers[:site].config["destination"],
-        "images/graphviz"
-      )
-      FileUtils.mkdir_p(destination) unless Dir.exists? destination
-      svg_name = "g-#{Digest::MD5.hexdigest(code)}.svg"
-      svg = File.join(destination, svg_name)
-      puts "dot -Tsvg -o #{svg}"
-      IO.popen("dot -Tsvg -o #{svg}", 'a') do |pipe|
-        pipe.puts(code)
-        pipe.close_write
+      code = super
+      title = if @title.empty? then DEFAULT_GRAPH_NAME else @title end
+
+      case @tag_name
+      when 'graphviz'	then render_graphviz code
+      when 'graph'	then render_graph 'graph', title, code
+      when 'digraph'	then render_graph 'digraph', title, code
+      else raise "unknown liquid tag name: #{@tag_name}"
       end
-      #svg = svg.lines.to_a[3..-1].join # Strip off the first two lines (doctype)
-      #svg
-      web_svg = "/images/graphviz/#{svg_name}"
-      "<embed class='graphviz' src='#{web_svg}' type='image/svg+xml' />"
+    end
+
+    def render_graphviz(code)
+      @src = code
+      svg = generate_svg code
+      filter_for_inline_svg svg
+    end
+
+    def filter_for_inline_svg(code)
+      code = remove_declarations code
+      code = remove_xmlns_attrs code
+      code = add_desc_attrs code
+      code = insert_desc_elements code
+      code = wrap_with_div code
+      code
+    end
+
+    def generate_svg code
+      Open3.popen3( DOT_CMD ) do |stdin, stdout, stderr|
+        stdout.binmode
+        stdin.print code
+        stdin.close
+
+        err = stderr.read
+        if not (err.nil? || err.strip.empty?)
+          raise "Error from #{DOT_CMD}:\n#{err}"
+        end
+
+        svg = stdout.read
+        svg.force_encoding 'UTF-8'
+
+        return svg
+      end
+    end
+
+    def remove_declarations(svg)
+      svg.sub(/<!DOCTYPE .+?>/im,'').sub(/<\?xml .+?\?>/im,'')
+    end
+
+    def remove_xmlns_attrs(svg)
+      svg.sub(%[xmlns="http://www.w3.org/2000/svg"], '')
+        .sub(%[xmlns:xlink="http://www.w3.org/1999/xlink"], '')
+    end
+
+    def add_desc_attrs(svg)
+      svg.sub!("<svg", %[<svg aria-label="#{CGI::escapeHTML @title}"])
+      svg.sub!("<svg", %[<svg role="img"])
+
+      return svg
+    end
+
+    def insert_desc_elements(svg)
+      inserted_elements =  %[<title>#{CGI::escapeHTML @title}</title>\n]
+      inserted_elements << %[<desc>#{CGI::escapeHTML @src}</desc>\n]
+      svg.sub!(/(<svg [^>]*>)/, "\\1\n#{inserted_elements}")
+
+      return svg
+    end
+
+    def wrap_with_div(svg)
+      %[<div class="#{DIV_CLASS_ATTR}">#{svg}</div>]
+    end
+
+    def render_graph(type, title, code)
+      render_graphviz %[#{type} "#{title}" { #{code} }]
     end
   end
 end
 
 Liquid::Template.register_tag('graphviz', Jekyll::GraphvizBlock)
+Liquid::Template.register_tag('graph', Jekyll::GraphvizBlock)
+Liquid::Template.register_tag('digraph', Jekyll::GraphvizBlock)
