e2db8b9faf6a9d0ea7af7817f23f480bf45c9e90
Russell Harmon <eatnumber1@google.com>
2016-08-02 02:30:34 -0700

    Fix github js

--- js/github.js
+++ js/github.js
@@ -3,7 +3,7 @@ function github_latest( count, username, elem, excludes ) {
 	$.yql(
 		/* TODO(eatnumber1): Delete this custom repo spec when https://github.com/yql/yql-tables/pull/478 is accepted. */
 		"USE 'https://raw.githubusercontent.com/eatnumber1/yql-tables/9dfd26064c4a3d2ea9fad3bcdf1086f2b2c2e836/github/github.user.repos.xml' AS github.user.repos;" +
-		"SELECT json.name, json.html_url, json.description, json.pushed_at FROM github.user.repos" +
+		"SELECT json.name, json.html_url, json.description, json.pushed_at FROM github.user.repos WHERE id='" + username + "'" +
 		excludes.map(function( elem, idx ) {
 			var ret = "", argname = "name" + id_idx++;
 			if( idx == 0 ) ret += " AND json.name NOT IN ( ";
